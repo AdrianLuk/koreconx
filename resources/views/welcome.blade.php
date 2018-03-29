725d87b66676c9c67eba6e20a5d694342f205de2
ADRIAN-PC\Adrian <aluk@playline.com>
2018-03-28 20:31:58 -0400

    navbar stuff

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,6 +9,8 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+            <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
 
         <!-- Styles -->
         <style>
@@ -19,6 +21,11 @@
                 font-weight: 100;
                 height: 100vh;
                 margin: 0;
+            }
+
+            .nav-link, .navbar-brand{
+                color: black;
+                font-weight: normal;
             }
 
             .full-height {
@@ -65,17 +72,10 @@
         </style>
     </head>
     <body>
+
+    @include('partials.navbar')
         <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
-                </div>
-            @endif
+            
 
             <div class="content">
                 <div class="title m-b-md">

02865ecaedc066ff28a793ffc622da276677723a
Adrian <lukabaseball@hotmail.com>
2018-03-31 01:47:05 -0400

    created EmailController

---
+++ app/Http/Controllers/EmailController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Http\Request;
+use App\User;
+use App\Email;
+
+class EmailController extends Controller
+{
+    //
+}
